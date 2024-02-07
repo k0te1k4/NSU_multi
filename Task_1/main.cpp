b70d882878ad243d0d4c6b7bf15d1d5b53968328
k0te1k4 <egor.gmag@yandex.ru>
2024-02-07 10:19:24 +0700

    Task 1 added

---
+++ Task_1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#ifdef USE_FLOAT_ARRAY
+typedef float arrayType;
+#else
+typedef double arrayType;
+#endif
+
+
+int main() {
+    const int N = 10000000;
+    arrayType pi = acos(-1.0);;
+
+    std::vector<arrayType> sinValues(N);
+    for (int i = 1; i < N; ++i) {
+        arrayType angle = 2*pi * i / N;
+        sinValues[i] = sin(angle);
+    }
+    arrayType sumValues = 0.0;
+    for (arrayType value:sinValues) {
+        sumValues+= value;
+    }
+    std::cout<< sumValues << std::endl;
+    return 0;
+}
